5d2732d9a3c7d09e7e9c81a68489958740204bed
chriswolfdesign <chriswolfdesign@gmail.com>
2022-06-03 15:09:29 -0400

    Added mock tests

---
+++ src/foo_test.go
@@ -0,0 +1,14 @@
+package main_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Foo", func() {
+	Describe("Tests", func() {
+		It("Should break", func() {
+			Expect(true).To(Equal(false))
+		})
+	})
+})
