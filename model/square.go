43d5ee4dde93ea489658d4b9eccaa64421ba014b
chriswolfdesign <chriswolfdesign@gmail.com>
2022-06-03 17:12:31 -0400

    Tested Square.GetPerimeter()

---
+++ model/square.go
@@ -0,0 +1,18 @@
+package model
+
+type Square struct {
+	rect Rectangle
+}
+
+func MakeSquare(side int) Square {
+	rect := MakeRectangle(side, side)
+	return Square{rect: rect}
+}
+
+func (s Square) GetPerimeter() float64 {
+	return s.rect.GetPerimeter()
+}
+
+func (s Square) GetArea() float64 {
+	return s.rect.GetArea()
+}
