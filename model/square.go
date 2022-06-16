db7ad1ea935c3ed602092b59661b120d4f916eb6
Chris Wolf <chriswolfdesign@gmail.com>
2022-06-16 18:38:37 -0400

    Refactored out unneeded functions in Square

--- model/square.go
+++ model/square.go
@@ -1,22 +1,9 @@
 package model
 
 type Square struct {
-	rect Rectangle
+	Rectangle
 }
 
 func MakeSquare(side int) Square {
-	rect := MakeRectangle(side, side)
-	return Square{rect: rect}
-}
-
-func (s Square) GetPerimeter() float64 {
-	return s.rect.GetPerimeter()
-}
-
-func (s Square) GetArea() float64 {
-	return s.rect.GetArea()
-}
-
-func (s Square) DisplayStats() {
-	s.rect.DisplayStats()
+	return Square{MakeRectangle(side, side)}
 }
